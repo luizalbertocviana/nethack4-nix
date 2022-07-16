262d8d489091006f487ab866da757fbfc9a20fcc
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-07-16 10:36:10 -0300

    first versions of `derivation.nix` and `.gitignore`

---
+++ default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+
+pkgs.stdenv.mkDerivation {
+  pname = "nethack4";
+  version = "16ba325";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "tsadok";
+    repo = "nethack4";
+    rev = "16ba32578f2a614b240ce849923f9053b198946d";
+    sha256 = "Ba0vsXup8kZ4LwUHCPtjU+LU1rAFKbkf9ltXvwFBxRA=";
+  };
+
+  buildInputs = [
+    pkgs.zlib
+    pkgs.flex
+    pkgs.bison
+  ];
+
+  buildPhase = ''
+    make BINDIR=$out
+  '';
+
+  installPhase = ''
+    make install BINDIR=$out
+  '';
+}
